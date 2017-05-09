50bebe1491b426c3f3132b599b34c2611e4259b6
Luiz <luizfrnnd412@hotmail.com>
2017-05-09 14:50:45 -0300

    Tentando estabelecer conexão com o banco

--- com/php/controller/conexaoComBanco/ConectaAoMySQL.php
+++ com/php/controller/conexaoComBanco/ConectaAoMySQL.php
@@ -1,4 +1,15 @@
 <?php
-$con = mysql_connect('NOMEDOHOST','NOMEDEUSUARIO','SENHA');
-mysql_select_db('BANCODEDADOS', $con)
+$banco = "nome_do_banco_de_dados";
+$usuario = "nome_de_usuario_criado";
+$senha = "senha_atribuida_para_o_bd";
+$hostname = "servidor_de_banco_de_dados";
+$conn = mysql_connect ( $hostname, $usuario, $senha );
+mysql_select_db ( $banco ) or die ( "Não foi possível conectar ao banco MySQL" );
+if (! $conn) {
+	echo "Não foi possível conectar ao banco MySQL.";
+	exit ();
+} else {
+	echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!";
+}
+mysql_close ();
 ?>
