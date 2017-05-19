35e1b70af4be510aed89b4c26a1c23eeb7993f1d
Luiz <luizfrnnd412@hotmail.com>
2017-05-19 19:39:08 -0300

    qw

--- formularios/jogador/Jogador.php
+++ formularios/jogador/Jogador.php
@@ -9,7 +9,7 @@
 	href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
 <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
 <script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
-
+<!-- hgghghg  -->
 <style></style>
 <link rel="stylesheet" type="text/css" href="../estilos.css">
 
